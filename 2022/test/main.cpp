5009d1ebc7258a7ca98d2e9375ef9b9b75d1ce81
yancey <yancey@whist.com>
2022-12-18 14:03:36 -0500

    still bug

--- 2022/test/main.cpp
+++ 2022/test/main.cpp
@@ -7,15 +7,14 @@ int test=0;
 struct pnt_t {
     int y,x;
     pnt_t () =default;
-    //pnt_t (const pnt_t &b):y(b.y),x(b.x){}OD
     pnt_t (int a,int b):y(a),x(b){}
-    /*pnt_t &operator =(const pnt_t &b) {
-	this->y=b.y;this->x=b.x;
-	return *this;
-	}*/
 
     bool operator == (pnt_t b) const{
-	return y==b.y&& x==b.x;
+	printf("compare!! %d %d\n",b.y,b.x);
+	//printf("compare! %d %d %d %d\n",y,x,b.y,b.x);
+	int r= y==b.y&& x==b.x;
+	//printf("<%d>",r);
+	return r;
     }
     
     bool operator < (const pnt_t &b) const{
@@ -26,6 +25,13 @@ struct pnt_t {
 
 
 struct pnt_hash{
+    size_t operator () (const pnt_t & a) const
+	{
+	    //return a.y+a.x;
+	    return a.y*1000000007 +a.x;
+	}
+};
+struct pnt_hash2{
     size_t operator () (const pnt_t & a) const
 	{
 	    //return a.y+a.x;
@@ -78,6 +84,7 @@ struct room_t
     
     void ins(pnt_t a,int type){
 	for(auto rr: shapes[type].pnts) {
+	    printf("---begin---\n");
 	    pnt_t ab= pnt_t{a.y+rr.y,a.x+rr.x};
 	    //printf("<ins %d %d>\n",ab.y,ab.x);
 	    ub=min(ub, ab.y);
@@ -91,14 +98,14 @@ struct room_t
 		printf("<after=%d before=%d   %d,%d   h=%ld>", after,before,ab.y,ab.x,  h(ab));
 	    }
 	    assert(after==before+1);
+	    printf("----end----\n\n");
 	}
     }
 
     void gc2() {
-	int magic=10;
-	decltype(st) tmp;
+	unordered_set<pnt_t,pnt_hash> tmp;
 	for(auto e:st) {
-	    if(e.y <= ub+magic) tmp.insert(e);
+	    tmp.insert(e);
 	}
     }
 
@@ -139,7 +146,7 @@ int main(){
 
     long left=1000000000000;
     for(int i=0;left>0;i++,left--){
-	room.gc2();
+
 	
 	int type= i%5;
 	pnt_t now = pnt_t{room.ub-4, 2};
@@ -147,6 +154,7 @@ int main(){
 	{
 	    pnt_t next2= pnt_t {now.y+1,now.x};
 	    if(room.conflict(next2,type)) {
+		assert(!room.conflict(now,type));
 		room.ins(now,type);
 		break;
 	    }else {
@@ -155,5 +163,6 @@ int main(){
 	    }
 	}
     }
+    room.gc2();
     return 0;
 }
